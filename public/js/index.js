54dbaf90421bfc86c0896ca95a178496260c8a6e
lyx <li.yunxi@northeastern.edu>
2022-11-01 05:59:22 -0700

    update url

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
-const deployURL="http://localhost:3000/";
-// const deployURL="https://intense-lowlands-69751.herokuapp.com/";
+//const deployURL="http://localhost:3000/";
+const deployURL="https://intense-lowlands-69751.herokuapp.com/";
 
 const loginURL = deployURL+'api/login/';
 
